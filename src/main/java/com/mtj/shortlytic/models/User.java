1f414ad962e0210dc19e04c3025ca2ec3018b01b
Somye <mahajan.somye@gmail.com>
2024-12-14 18:48:01 +0530

    setup

---
+++ src/main/java/com/mtj/shortlytic/models/User.java
@@ -0,0 +1,34 @@
+package com.mtj.shortlytic.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private UUID id;
+
+    @NotEmpty
+    @NotNull
+    @Size(max = 255, message = "Username length cannot exceed 255!")
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    @Size(max = 255, message = "Password length cannot exceed 255!")
+    private String password;
+
+    @Email
+    @NotNull
+    @NotEmpty
+    @Size(max = 255, message = "Email length cannot exceed 255!")
+    private String email;
+}
