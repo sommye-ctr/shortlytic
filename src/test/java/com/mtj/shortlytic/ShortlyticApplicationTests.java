2d63b010b06e3d8fbd2aacc8cee05c83bd55b9de
Somye <mahajan.somye@gmail.com>
2024-12-14 17:38:09 +0530

    Initial commit

---
+++ src/test/java/com/mtj/shortlytic/ShortlyticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtj.shortlytic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlyticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
