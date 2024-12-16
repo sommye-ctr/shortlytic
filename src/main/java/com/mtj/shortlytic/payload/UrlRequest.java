33fdcc60f9c3e53e4d6dce0adf32059f93ec0bcc
Somye <mahajan.somye@gmail.com>
2024-12-16 13:43:18 +0530

    Added validations in DTO

--- src/main/java/com/mtj/shortlytic/payload/UrlRequest.java
+++ src/main/java/com/mtj/shortlytic/payload/UrlRequest.java
@@ -1,10 +1,13 @@
 package com.mtj.shortlytic.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Setter
 @Getter
 public class UrlRequest {
+    @NotNull @NotEmpty
     private String url;
     private String password;
     private boolean passwordProtected;
--- src/main/java/com/mtj/shortlytic/payload/UserRequest.java
+++ src/main/java/com/mtj/shortlytic/payload/UserRequest.java
@@ -1,12 +1,24 @@
 package com.mtj.shortlytic.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class UserRequest {
+    @NotEmpty @NotNull
+    @Size(max = 255, message = "Username length cannot exceed 255!")
     private String username;
+
+    @NotNull @NotEmpty
+    @Size(max = 255, message = "Password length cannot exceed 255!")
     private String password;
+
+    @Email @NotNull @NotEmpty
+    @Size(max = 255, message = "Email length cannot exceed 255!")
     private String email;
 }
